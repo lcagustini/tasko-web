d1094c8e88e960080cb5a97ec351a0da5e8f3e6b
lcagustini <torresmon235@gmail.com>
2018-04-08 17:56:59 -0300

    Functional Front-end; Labels

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+function once(fn){
+    let returnValue;
+    let canRun = true;
+    return function runOnce(){
+        if(canRun) {
+            returnValue = fn.apply(this, arguments);
+            canRun = false;
+        }
+        return returnValue;
+    }
+}
+
+function throttle(fn, interval) {
+    let lastTime;
+    return function throttled() {
+        let timeSinceLastExecution = Date.now() - lastTime;
+        if(!lastTime || (timeSinceLastExecution >= interval)) {
+            fn.apply(this, arguments);
+            lastTime = Date.now();
+        }
+    };
+}
+
+function getBrightness(hexCode) {
+    hexCode = hexCode.replace('#', '');
+
+    var c_r = parseInt(hexCode.substr(0, 2),16);
+    var c_g = parseInt(hexCode.substr(2, 2),16);
+    var c_b = parseInt(hexCode.substr(4, 2),16);
+
+    return 1 - ( 0.299 * c_r + 0.587 * c_g + 0.114 * c_b)/255;
+}
+
+function requestJSON(method, url, data, callback){
+    console.log("Request --\nMethod:"+method+"\tUrl:"+url+"\nData:\n"+data);
+
+    let request = new XMLHttpRequest();
+    request.open(method, url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onreadystatechange = () => {
+        if(request.readyState == 4 && (request.status == 204 || request.status == 200)){
+            callback(request);
+        }
+    };
+    if(data){
+        request.send(data, true);
+    }
+    else{
+        request.send(true);
+    }
+}
+
 function newList(){
     let currentBoard = document.querySelector("#board-title").innerHTML;
 
@@ -15,15 +67,11 @@ function newList(){
         page.querySelector("#new-list-form").addEventListener("submit", (e) => {
             e.preventDefault();
 
-            let request = new XMLHttpRequest();
-            request.open("POST", "/new/list");
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.onreadystatechange = () => {
-                if(request.readyState == 4 && request.status == 204){
-                    updateApp();
-                }
-            };
-            request.send(`{ \"name\": \"${page.querySelector("#new-list-name").value}\", \"board\": \"${currentBoard}\" }`, true);
+            requestJSON("POST",
+                "/new/list",
+                `{ \"name\": \"${page.querySelector("#new-list-name").value}\", \"board\": \"${currentBoard}\" }`,
+                updateApp
+            );
 
             input.blur();
         });
@@ -47,21 +95,48 @@ function newBoard(){
     navbar.querySelector("#new-board-form").addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let request = new XMLHttpRequest();
-        request.open("POST", "/new/board");
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.onreadystatechange = () => {
-            if(request.readyState == 4 && request.status == 204){
-                updateApp();
-            }
-        };
-        request.send(`{ \"name\": \"${navbar.querySelector("#new-board-name").value}\" }`, true);
+        requestJSON("POST",
+            "/new/board",
+            `{ \"name\": \"${navbar.querySelector("#new-board-name").value}\" }`,
+            updateApp
+        );
 
         input.blur();
     });
     input.focus();
 
     componentHandler.upgradeAllRegistered();
+}
+
+function newItem(e){
+    document.querySelector(".mdl-dialog").showModal();
+
+    let currentList = e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
+    let item = (e) => {
+        let currentBoard = document.querySelector("#board-title").innerHTML;
+        let name = document.querySelector("#item-name").value;
+        let note = document.querySelector("#item-note").value;
+
+        requestJSON("POST",
+            "/new/item",
+            `{ \"name\": \"${name}\", \"board\": \"${currentBoard}\", \"list\": \"${currentList}\", \"note\": \"${note}\" }`,
+            () => { document.querySelector(".mdl-dialog").close(); updateApp(); }
+        );
+
+        document.querySelector("#add-item-button").removeEventListener("click", item);
+    }
+
+    document.querySelector("#add-item-button").addEventListener("click", item);
+}
+
+function delBoard(){
+    let currentBoard = document.querySelector("#board-title").innerHTML;
+
+    requestJSON("DELETE",
+        "/del/board",
+        `{ \"name\": \"${currentBoard}\" }`,
+        (e) => {updateApp(e); updateListsForBoard("");}
+    );
 }
 
 function updateListsForBoard(name){
@@ -79,44 +154,32 @@ function updateListsForBoard(name){
         list.querySelector(".mdl-menu").setAttribute("for", "header["+i+"]");
         list.querySelector(".mdl-button").setAttribute("id", "header["+i+"]");
 
-        list.querySelector("#add-item").addEventListener("click", (e) => {
-            document.querySelector(".mdl-dialog").showModal();
-
-            let currentList = e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
-            let newList = (e) => {
-                let currentBoard = document.querySelector("#board-title").innerHTML;
-                let name = document.querySelector("#item-name").value;
-                let note = document.querySelector("#item-note").value;
-
-                let request = new XMLHttpRequest();
-                request.open("POST", "/new/item");
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.onreadystatechange = () => {
-                    if(request.readyState == 4 && request.status == 204){
-                        document.querySelector(".mdl-dialog").close();
-                        updateApp();
-                    }
-                };
-                request.send(`{ \"name\": \"${name}\", \"board\": \"${currentBoard}\", \"list\": \"${currentList}\", \"note\": \"${note}\" }`, true);
-
-                document.querySelector("#add-item-button").removeEventListener("click", newList);
-            }
-
-            document.querySelector("#add-item-button").addEventListener("click", newList);
-        });
+        list.querySelector("#add-item").addEventListener("click", newItem);
 
         list.querySelector("#delete-list").addEventListener("click", (e) => {
             let currentBoard = document.querySelector("#board-title").innerHTML;
+            let currentList = e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
 
-            let request = new XMLHttpRequest();
-            request.open("DELETE", "/del/list");
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.onreadystatechange = () => {
-                if(request.readyState == 4 && request.status == 204){
-                    updateApp();
+            requestJSON("DELETE",
+                "/del/list",
+                `{ \"name\": \"${currentList}\", \"board\": \"${currentBoard}\" }`,
+                updateApp
+            );
+        });
+
+        list.querySelector("#delete-item").addEventListener("click", (e) => {
+            let currentBoard = document.querySelector("#board-title").innerHTML;
+            let currentList = e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
+
+            for(item in json[currentBoard][currentList]){
+                if(json[currentBoard][currentList][item].checked){
+                    requestJSON("DELETE",
+                        "/del/item",
+                        `{ \"name\": \"${item}\", \"board\": \"${currentBoard}\", \"list\": \"${currentList}\" }`,
+                        updateApp
+                    );
                 }
-            };
-            request.send(`{ \"name\": \"${key}\", \"board\": \"${currentBoard}\" }`, true);
+            }
         });
 
         i++;
@@ -126,23 +189,29 @@ function updateListsForBoard(name){
             let row = document.importNode(document.querySelector("#row-template").content, true);
             row.querySelector(".mdl-checkbox").setAttribute("for", "row["+i+"]");
             row.querySelector(".mdl-checkbox__input").setAttribute("id", "row["+i+"]");
-            row.querySelector(".mdl-data-table__cell--non-numeric").innerHTML = lists[key][text].name;
+            row.querySelector(".mdl-data-table__cell--non-numeric").prepend(lists[key][text].name);
+            for(let i = 0; i < lists[key][text].labels.length; i++){
+                let label = document.importNode(document.querySelector("#label-template").content, true);
+                label.children[0].innerHTML = lists[key][text].labels[i].name;
+                label.children[0].style.backgroundColor = "#"+(lists[key][text].labels[i].color.toString(16)).padStart(6, "0");
+                label.children[0].classList.add("mdl-color-text--"+
+                    (getBrightness(lists[key][text].labels[i].color.toString(16)) < 0.5 ? "black" : "white")
+                );
+
+                row.querySelector(".mdl-data-table__cell--non-numeric").appendChild(label);
+            }
             row.querySelector(".mdl-checkbox__input").checked = lists[key][text].checked;
 
             row.querySelector(".mdl-checkbox__input").addEventListener("click", (e) => {
                 let currentBoard = document.querySelector("#board-title").innerHTML;
-                let currentItem = e.target.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
+                let currentItem = e.target.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").firstChild.textContent;
                 let currentList = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".mdl-data-table__cell--non-numeric").innerHTML;
 
-                let request = new XMLHttpRequest();
-                request.open("PATCH", "/upd/check");
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.onreadystatechange = () => {
-                    if(!(request.readyState == 4 && request.status == 204)){
-                        updateApp();
-                    }
-                };
-                request.send(`{ \"name\": \"${currentItem}\", \"board\": \"${currentBoard}\", \"list\": \"${currentList}\" }`, true);
+                requestJSON("PATCH",
+                    "/upd/check",
+                    `{ \"name\": \"${currentItem}\", \"board\": \"${currentBoard}\", \"list\": \"${currentList}\" }`,
+                    updateApp
+                );
             });
 
             table.appendChild(row);
@@ -176,14 +245,12 @@ function updateBoards(){
 }
 
 function updateApp(){
-    document.querySelector(".mdl-dialog").querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".mdl-dialog").close();
-    });
+    setup();
 
-    let request = new XMLHttpRequest();
-    request.open("GET", "/json");
-    request.onreadystatechange = () => {
-        if(request.readyState == 4 && request.status == 200){
+    requestJSON("GET",
+        "/json",
+        null,
+        (request) => {
             json = JSON.parse(request.responseText);
             let currentBoard = document.querySelector("#board-title").innerHTML;
 
@@ -194,8 +261,32 @@ function updateApp(){
 
             console.log(json);
         }
-    };
-    request.send(true);
+    );
 }
+
+let setup = once(() => {
+    document.querySelector(".mdl-dialog").querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".mdl-dialog").close();
+    });
+
+    let toggleFAB = (e) => {
+        document.querySelector("#delete-board-button").classList.toggle("delete-board-up");
+        document.querySelector("#delete-board-button").classList.toggle("delete-board-down");
+
+        document.querySelector("#settings-button").classList.toggle("settings-up");
+        document.querySelector("#settings-button").classList.toggle("settings-down");
+    }
+
+    window.addEventListener("click", (e) => {
+        if(document.querySelector("#dashboard-button").contains(e.target)){
+            toggleFAB(e);
+        }
+        else{
+            if(document.querySelector("#delete-board-button").classList.contains("delete-board-up")){
+                toggleFAB(e);
+            };
+        }
+    });
+});
 
 updateApp();
